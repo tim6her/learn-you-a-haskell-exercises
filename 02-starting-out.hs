89d3ff62a99702026472ffbf8bdfb3da200adbae
Tim B. Herbstrith <tim.benedikt.herbstrith@univie.ac.at>
2017-01-28 22:32:38 +0100

    Solutions to chapter 1

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -10,14 +10,21 @@
  -}
 
 -- Find the penultimate element in list l
+penultimate :: [a] -> a
 penultimate l = last (init l)
 
 -- Find the element at index k in list l
 -- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+findK :: (Integral k) => k -> [a] -> a
+findK _ [] = error "Index out of range"
+findK k (x:xs)
+    | k < 0 = error "Negativ index"
+    | k == 0 = x
+    | otherwise = findK (k - 1) xs
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+isPalindrome :: (Eq a) => [a] -> Bool
+isPalindrome l = l == reverse l
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
@@ -26,31 +33,68 @@ isPalindrome l = undefined
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = undefined
+duplicate :: [a] -> [a]
+duplicate [] = []
+duplicate [x] = [x, x]
+duplicate (x:xs) = x:x:duplicate xs
 
 {-
  - Imitate the functinality of zip
  - The function "min x y" returns the lower of values x and y
  - For example "ziplike [1,2,3] ['a', 'b', 'c', 'd']" returns [(1,'a'), (2, 'b'), (3, 'c')]
  -}
-ziplike xs ys = undefined
+ziplike :: [a] -> [b] -> [(a, b)]
+ziplike [] _ = []
+ziplike _ [] = []
+ziplike (x:xs) (y:ys) = (x, y):ziplike xs ys
 
 -- Split a list l at element k into a tuple: The first part up to and including k, the second part after k
 -- For example "splitAtIndex 3 [1,1,1,2,2,2]" returns ([1,1,1],[2,2,2])
-splitAtIndex k l = undefined
+splitAtIndex :: (Integral k) => k -> [a] -> ([a], [a])
+splitAtIndex _ [] = error "Index out of range"
+splitAtIndex k all@(x:xs)
+    | k < 0 = error "Negativ index"
+    | k == 0 = ([], all)
+    | otherwise = ((x:h), t)
+        where (h, t) = splitAtIndex (k - 1) xs
 
 -- Drop the element at index k in list l
 -- For example "dropK 3 [0,0,0,1,0,0,0]" returns [0,0,0,0,0,0]
-dropK k l = undefined
+dropK :: (Integral k) => k -> [a] -> [a]
+dropK _ [] = error "Index out of range"
+dropK k all@(x:xs)
+    | k < 0 = error "Negativ index"
+    | k == 0 = xs
+    | otherwise = x:dropK (k - 1) xs
 
 -- Extract elements between ith and kth element in list l. Including i, but not k
 -- For example, "slice 3 6 [0,0,0,1,2,3,0,0,0]" returns [1,2,3]
-slice i k l = undefined
+slice :: (Integral i) => i -> i -> [a] -> [a]
+slice i k l
+    | i < 0 = error "Negativ start index"
+    | i > k = error "Start index must be smaller than end "
+    | k == 0 = []
+    | i == 0 = x:slice 0 (k - 1) xs
+    | otherwise = slice (i - 1) (k - 1) xs
+    where (x:xs) = l
 
 -- Insert element x in list l at index k
 -- For example, "insertElem 2 5 [0,0,0,0,0,0]" returns [0,0,0,0,0,2,0]
-insertElem x k l = undefined
+insertElem :: (Integral k) => a -> k -> [a] -> [a]
+insertElem x k l
+    | k < 0 = error "Negativ index"
+    | k == 0 = x:l
+    | length l == 0 = error "Index out of range"
+    | otherwise = y:insertElem x (k - 1) ys
+    where (y:ys) = l
 
 -- Rotate list l n places left.
 -- For example, "rotate 2 [1,2,3,4,5]" gives [3,4,5,1,2]
-rotate n l = undefined
+rotate :: (Integral n) => n -> [a] -> [a]
+rotate 0 l = l
+rotate _ [] = []
+rotate n l
+    | n < 0 = rotate (n + fromIntegral (length l)) l
+    | otherwise = rotate (n - 1)(las:ini)
+    where ini = init l
+          las = last l
